1a083f9cf0b5b5268f6534f5dd710662c34c8582
Mahmud256 <mahmud474849@gmail.com>
2023-09-25 08:42:20 +0600

    Include Search

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -4,6 +4,14 @@ const Banner = () => {
     return (
         <div>
             <h2 className="text-5xl font-bold text-center">I Grow By Helping People In Need</h2>
+            <div className="form-control">
+                <div className="input-group justify-center mt-12">
+                    <input type="text" placeholder="Search here...." className="input input-bordered" />
+                    <button className="btn btn-error normal-case text-[#fff]">
+                    Search
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
