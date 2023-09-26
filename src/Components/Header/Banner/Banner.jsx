e583cba1a7b43bc82da78e5d55885f55ca548573
Mahmud256 <mahmud474849@gmail.com>
2023-09-26 10:58:20 +0600

    Include Background Image

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const Banner = () => {
     return (
         <div>
-            <div className="h-[100vh] bg-center bg-cover bg-[url('./assets/doctor.jpg')] bg-no-repeat">
-                <div className='flex justify-center items-center flex-col h-full'>
+            <div className="relative h-[100vh] bg-center bg-no-repeat">
+                <div className="absolute bg-cover inset-0 bg-[url('./assets/doctor.jpg')] backdrop-blur-md opacity-10 bg-no-repeat"></div>
+                <div className='relative flex justify-center items-center flex-col h-full'>
                     <h2 className="text-3xl lg:text-5xl font-bold text-center">I Grow By Helping People In Need</h2>
                     <div className="form-control">
                         <div className="input-group justify-center mt-12">
