03362fc580c0c3e790d4ffbaca5fd8e41b19a3b1
Mahmud256 <mahmud474849@gmail.com>
2023-09-26 16:27:33 +0600

    Remove See Less Button

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -2,53 +2,46 @@ import React, { useEffect, useState } from 'react';
 import DonationCard from './DonationCard';
 
 const Donation = () => {
-
-    const [donations, setDonations] = useState([])
-
-    console.log(donations.length);
-
-    const [noFound, setNofound] = useState(false)
-
-    const [isSeeAll, setIsSeeAll] = useState(false)
+    const [donations, setDonations] = useState([]);
+    const [noFound, setNofound] = useState(false);
+    const [isSeeAll, setIsSeeAll] = useState(false);
 
     useEffect(() => {
         const donationItems = JSON.parse(localStorage.getItem('donations'));
 
         if (donationItems) {
             setDonations(donationItems);
-        }
-        else {
+        } else {
             setNofound('No Data Found');
         }
-    }, [])
+    }, []);
 
     return (
         <div>
-            {noFound ? <p className="h-[80vh] flex justify-center items-center">{noFound}</p> :
-
+            {noFound ? (
+                <p className="h-[80vh] flex justify-center items-center">{noFound}</p>
+            ) : (
                 <div className='flex justify-center'>
                     <div>
                         <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-                            {
-                                isSeeAll ? donations.map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
-                                    :
-                                    donations.slice(0, 4).map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
+                            {isSeeAll
+                                ? donations.map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
+                                : donations.slice(0, 4).map(donation => <DonationCard key={donation.id} donation={donation}></DonationCard>)
                             }
                         </div>
 
-                        {
-                            donations.length > 4
-                            &&
+                        {donations.length > 4 && (
                             <div className='my-6'>
-                                <button onClick={() => setIsSeeAll(!isSeeAll)} className={`p-2 block rounded-lg mx-auto ${isSeeAll ? 'bg-orange-700 text-white' : 'bg-green-500 text-white'
-                                    }`}>{isSeeAll ? 'See Less' : 'See All'}</button>
+                                {!isSeeAll && (
+                                    <button onClick={() => setIsSeeAll(true)} className='p-2 block rounded-lg mx-auto bg-green-500 text-white'>
+                                        See All
+                                    </button>
+                                )}
                             </div>
-                        }
+                        )}
                     </div>
-
                 </div>
-
-            }
+            )}
         </div>
     );
 };
